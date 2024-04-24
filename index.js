ab38742f98e1127a61b24b05ce35c0d23170af17
TAMARAPREYE BRABOKE <prudentbraboke@gmail.com>
2024-04-24 11:27:08 +0100

    modified origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["https://may-mart.vercel.app", "http://localhost:5173"],
+    origin: ["https://may-mart.vercel.app/", "http://localhost:5173"],
     credentials: true,
     // methods: ["GET", "POST", "PUT", "DELETE"],
   })
